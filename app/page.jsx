2c15a3dabe755beb26aaebe34ac199b5c14b0dbd
glruin <antokwghqm@gmail.com>
2025-11-30 02:35:12 -0300

    Change background image from fondo.jpg to fondo.png

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div
       style={{
-        backgroundImage: "url('/fondo.jpg')",
+        backgroundImage: "url('/fondo.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
